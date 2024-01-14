520acd7eb2686cfe50e69fcbbf6fa51fee4ffc36
laeswo <laeswo@gmail.com>
2024-01-14 23:16:57 +0900

    d
    d

---
+++ Assets/networkmanager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon;
+using Photon.Pun;
+using UnityEngine.UI;
+
+public class networkmanager : MonoBehaviourPunCallbacks
+{
+    public Text hp1;
+    public Text hp2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void connct()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+}
