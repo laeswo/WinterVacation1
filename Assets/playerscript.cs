a1d31c6ecb7f53bd4bcdd6f44489340f72ee710b
laeswo <laeswo@gmail.com>
2024-01-15 00:47:09 +0900

    d
    d

--- Assets/playerscript.cs
+++ Assets/playerscript.cs
@@ -4,8 +4,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using UnityEngine.UI;
+
 public class playerscript : MonoBehaviourPunCallbacks, IPunObservable
 {
+    public Image hpbar;
+    public Text nickname;
+    public Text nickname_enemey;
+    public PhotonView pv;
+    public Rigidbody2D rg;
+
+    void Update()
+    {
+        nickname.text = PhotonNetwork.NickName;
+        nickname.text = pv.Owner.NickName;
+    }
+        
+
+    
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
    
