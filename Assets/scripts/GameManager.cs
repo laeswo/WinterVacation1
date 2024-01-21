43a906751a01e42a2b98b3cc01cb66aecb2e5c34
minyee2913 <leegm0759@gmail.com>
2024-01-21 14:14:52 +0900

    system

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
+using UnityEngine.UI;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    bool isWaiting;
+    public Text cause;
+    public Image loading;
+    public Image cover;
     void Start()
     {
-        
+        isWaiting = true;
+        loading.gameObject.SetActive(true);
+        cause.text = "상대 플레이어의 로딩이 끝날 때까지 대기합니다...";
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (isWaiting) {
+            if (PhotonNetwork.PlayerListOthers.Length < 1) {
+                StartCoroutine(Disconnected());
+            } else {
+                Hashtable ht = PhotonNetwork.PlayerListOthers[0].CustomProperties;
+                // if (ht["state"] != "loading") {
+
+                // }
+            }
+        }
+    }
+
+    IEnumerator Disconnected() {
+        cover.gameObject.SetActive(true);
+        cause.text = "상대 플레이어의 연결이 끊켰습니다.\n게임이 종료됩니다...";
+        loading.gameObject.SetActive(true);
+
+        PhotonNetwork.Disconnect();
+
+        yield return new WaitForSeconds(1.5f);
+
+        LoadingController.LoadScene("lobby");
     }
 }
